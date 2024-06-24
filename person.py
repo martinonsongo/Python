9821a941d4f67fc86bd0e99a27b294bb64122f55
martinonsongo <hondiekimartin2018@gmail.com>
2024-06-24 14:08:42 +0300

    Add files via upload

---
+++ person.py
@@ -0,0 +1,7 @@
+class person:
+    def __init__(self,name,age,gender):
+        self.name=name
+        self.age=age
+        self.gender=gender
+obj1=person('martin',23,'M')
+print('this person is ',obj1.name,'your age is  ',obj1.age,'gender is  ',obj1.gender,'.')
